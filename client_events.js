7c8102ad88691d0394bc2d147eb38ec54ec3948d
Karl Glaser <kg@papercloud.com.au>
2014-02-17 02:57:59 +1100

    refactor

--- client_events.js
+++ client_events.js
@@ -1,9 +1,6 @@
 var clientPinger = require('./client_pinger');
 exports.process = function(event) {
     if (event.data === 'pong') {
-        document.querySelector('#ping').innerHTML = new Date() - clientPinger.start + "ms";
-        setTimeout(function() {
-            clientPinger.ping(event.currentTarget);
-        }, 1000);
+        clientPinger.pong(event);
     }
 };
--- client_pinger.js
+++ client_pinger.js
@@ -1,4 +1,13 @@
+exports.pingRate = 13000;
+
 exports.ping = function(ws) {
 	this.start = new Date();
 	ws.send('ping');
+};
+
+exports.pong = function(event) {
+	document.querySelector('#ping').innerHTML = new Date() - this.start + "ms";
+	        setTimeout(function() {
+	            this.ping(event.currentTarget);
+	        }.bind(this), this.pingRate);
 };
